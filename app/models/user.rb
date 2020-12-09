8be303643fbc9cb012f2466053f7bca21cca8027
koukimaedomari <0uh3263022m0t8c@ezweb.ne.jp>
2020-12-09 19:16:52 +0900

    commit

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  # validates :name,               presence: true
+  # validates :email,              presense: true
+  # validates :encrypted_password, presence: true
+  # validates :last_name,          presence: true
+  # validates :first_name,         presence: true
+  # validates :kana_last_name,     presence: true
+  # validates :kana_first_name,    presence: true
+  # validates :birth_day,          presence: true
+end
