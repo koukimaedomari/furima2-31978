73d0b8021d7727c69ff007b80b1be569f662d554
koukimaedomari <0uh3263022m0t8c@ezweb.ne.jp>
2020-12-19 19:52:10 +0900

    アクティブハッシュとアクティブストレージの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_text, :image, :category_id, :status_id, :postage_id, :area_id, :day_id, :price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,28 @@
 class Item < ApplicationRecord
   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
+  
+  with_options presence: true do
+    validates :item_name
+    validates :item_text
+    validates :image
+  
+    
+
+  end
+  
+
+
+
+
+
+  belongs_to :user
+  has_one :buy
+  has_one_attached :image
+  
 end
